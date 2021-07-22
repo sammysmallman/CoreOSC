e289069ea9c020809f873aa916d6cff7e1240961
Sam Smallman <srsmallman@mac.com>
2021-07-22 11:50:23 +0100

    Added core OSC objects

---
+++ Tests/CoreOSCTests/CoreOSCTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+    @testable import CoreOSC
+
+    final class CoreOSCTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+
+        }
+    }
